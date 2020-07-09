a2b51bb11a05f4f037e733d3e76111eaead2c90d
ruchika <ruchika6698@gmail.com>
2020-07-09 08:19:20 +0530

    Added:Negative test case for forgot password

--- src/FundooTest/forgotpassword.test.js
+++ src/FundooTest/forgotpassword.test.js
@@ -21,3 +21,22 @@ describe('Email input', () => {
         expect(wrapper.state('email')).toEqual('Diksha.Rane@gmail.com');
         })
        })
+
+describe('forgotpassword Component', () => {
+
+    // make our assertion and what we expect to happen 
+    it('should render without throwing an error', () => {
+        expect(shallow( < ForgotPassword />).exists()).toBe(true)
+    })
+})
+
+describe('Email input', () => {
+  
+        it('should respond to change event and change the state of the Forgot Password Component', () => {
+         
+         const wrapper = shallow( < ForgotPassword />);
+         wrapper.find('#outlined-required').simulate('change', {target: {name: 'email', value: 'Diksha@gmail.com'}});
+         
+        expect(wrapper.state('email')).not.toEqual('Diksha.Rane@gmail.com');
+        })
+       })
